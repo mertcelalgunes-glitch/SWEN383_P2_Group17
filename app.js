9e76f29386e3eb43c3e0828891b05d75ae83adc0
mertcelalgunes-glitch <mert.celal.gunes@gmail.com>
2025-12-04 18:11:41 +0000

    Some basic methods done

--- app.js
+++ app.js
@@ -63,3 +63,57 @@ class User {
         this.email = email;
     }
 }
+
+class RepositoryFactory {
+    static createRepository(type) {
+        switch(type) {
+            case 'memory':
+                return new MemoryRepository();
+            default:
+                throw new Error('Unkown repository type ${type}');
+        }
+    }
+}
+
+class MemoryRepository {
+    constructor() {
+        this.recipes = new Map();
+        this.mealPlans = new Map();
+        this.users = new Map();
+        this.currentUserId = null;
+    }
+
+    saveRecipe(recipe) {
+        if (!recipe.id) {
+            recipe.id = 'recipe-${Date.now()}-${Math.random().toString(36).substr(2, 9)}';
+        }
+        this.recipes.set(recipe.id, recipe)
+        return recipe.id;
+    }
+
+    getRecipe(id) {
+        return this.recipes.get(id);
+    }
+    getAllRecipes() {
+        return Array.from(this.recipes.values());
+    }
+
+    deleteRecipe(id) {
+        return this.recipes.delete(id);
+    }
+
+    saveMealPlan(mealPlan){
+        if (!mealPlan.id) {
+            mealPlan.id = 'plan-${Date.now()}-${Math.random().toString(36).substr(2, 9)}';
+        }
+        this.mealPlans.set(mealPlan.id, mealPlan);
+        return mealPlan.id;
+    }
+getMealPlan(id) {
+    return this.mealPlans.get(id);
+}
+
+getUserMealPlans(userId) {
+    return Array.from(this.mealPlans.values()).filter(plan => plan.userId === userId);
+}
+}
